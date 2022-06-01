6e987d41f5fec5385aa68873dcd4e342b62c1b28
345dvl <k.f.kntn@gmail.com>
2022-06-02 00:03:24 +0900

    :+1: getUserテスト

--- app/user.go
+++ app/user.go
@@ -1,17 +1,16 @@
 package controller
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-)
-
 type User struct {
 	Ñame  string
 	Email string
 }
 
-func getUser(c echo.Context) error {
-	user := User{"hato", "k.f.kntn@gmail.com"}
-	return c.JSON(http.StatusOK, user)
+// func getUser(c echo.Context) error {
+// 	user := User{"hato", "k.f.kntn@gmail.com"}
+// 	return c.JSON(http.StatusOK, user)
+// }
+
+func getUser() (user User) {
+	user = User{"hato", "k.f.kntn@gmail.com"}
+	return
 }
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	controller "github.com/345dvl/bookshelf-back/controller"
 )
 
 func main() {
@@ -12,5 +13,6 @@ func main() {
 		return c.String(http.StatusOK, "Hello, Air on Docker!!")
 	})
 	// e.GET("/user", controller.getUser)
+	controller.getUser()
 	e.Logger.Fatal(e.Start(":8080"))
 }
